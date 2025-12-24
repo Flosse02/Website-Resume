0c235e08204be588d3d4f27b6e366d815d8203b3
Ewan <vandenator10@gmail.com>
2025-12-24 12:29:46 +0800

    Added new js that holds all information about my projects, to be used to expand how I show my projects

---
+++ data/projects.js
@@ -0,0 +1,43 @@
+const projects = [
+  {
+    id: "resume",
+    title: "Online Resume",
+    category: "UI/UX Design",
+    image: "images/onlineResume.jpg",
+    description: "An online resume built using html and modern UI/UX practices, to show off my skill along with my qualifications.",
+    features: [
+      "Responsive layout",
+      "Modern design principles",
+      "Easy navigation"
+    ],
+    github: "https://github.com/Flosse02/Website-Resume"
+  },
+
+  {
+    id: "smartHome",
+    title: "Smarthome Device",
+    category: "UI/UX + Electrical Engineering",
+    image: "images/smartHome.jpg",
+    description: "A basic IOT based smarthome with google calendar integration, music playback and basic home automation. (ABANDONED)",
+    features: [
+      "Custom PCB",
+      "ESP-based control",
+      "Mobile-friendly dashboard"
+    ],
+    github: "https://github.com/Flosse02/smarthome"
+  },
+
+  {
+    id: "smartHomeV2",
+    title: "Smarthome Device V2",
+    category: "UI/UX + Electrical Engineering",
+    image: "images/smartHomeV2.jpg",
+    description: "A basic IOT based smarthome with google calendar integration, music playback and basic home automation. This is the second version with improved features and design. (WIP)",
+    features: [
+      "Custom PCB",
+      "ESP-based control",
+      "Mobile-friendly dashboard"
+    ],
+    github: "https://github.com/Flosse02/smartHomeV2"
+  }
+];
